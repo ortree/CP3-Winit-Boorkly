56a2e8ce377067c9072c5bff14826d92a8a53d49
ortree01 <ortree01@gmail.com>
2020-11-08 16:30:53 +0700

    finish ex4

--- Exercise4_Winit_B.py
+++ Exercise4_Winit_B.py
@@ -1,2 +1,9 @@
-x=10
-print(x)
+fe_score = 50
+ge_score = 60
+ic_score = 70
+cp_score = 40
+print("--- Your Score ---")
+print("Foundation English: ", fe_score)
+print("General Business: ", ge_score)
+print("Introduction to Computer System: ",ic_score)
+print("Computer Programming: ",cp_score)
