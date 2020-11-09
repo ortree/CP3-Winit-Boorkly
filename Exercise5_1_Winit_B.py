9b0ad1dfb9f8bceb363484f43fac357f3202c21e
ortree01 <ortree01@gmail.com>
2020-11-09 11:22:59 +0700

    Ex5

---
+++ Exercise5_1_Winit_B.py
@@ -0,0 +1,9 @@
+print("Calculator.............")
+a = int(input("fill first number :"))
+b = int(input("fill second number :"))
+
+print("All Result ............. ")
+print(a,"+",b," = ",a+b)
+print(a,"-",b," = ",a-b)
+print(a,"*",b," = ",a*b)
+print(a,"/",b," = ",a/b)
---
+++ Exercise5_2_Winit_B.py
@@ -0,0 +1,6 @@
+print("Calculator object movement.............")
+distance = int(input("fill distance : "))
+time =  int(input("fill time :"))
+
+print("Result of Object Movement.......")
+print(distance/time , " km/h")
